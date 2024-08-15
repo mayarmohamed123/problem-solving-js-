3d12d6ccc3bddd91da906a3a742fddb5d7e9ea63
mayar mohamed <mayar201500@gmail.com>
2024-08-16 01:48:05 +0300

    Adding some problems in 16-8-2024

---
+++ Calculate BMI.js
@@ -0,0 +1,20 @@
+// Description:
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+function bmi(weight, height) {
+  const bm = weight / (height * height);
+  return bm <= 18.5
+    ? "Underweight"
+    : bm <= 25.0
+    ? "Normal"
+    : bm <= 30.0
+    ? "Overweight"
+    : "Obese";
+}
